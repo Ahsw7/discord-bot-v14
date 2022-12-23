a6aa3749c1deba6d6dfdb70b2e20c4cc472306dc
Ahsw7 <ahsw75@gmail.com>
2022-12-23 16:00:24 +0700

    getting started with canvas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const { Client, IntentsBitField, Collection } = require("discord.js");
 const { readdirSync } = require("fs");
 const path = require("path");
+const { registerFont } = require("canvas");
+
+const Constants = require("./util/Constants");
 
 const intents = Object.values(IntentsBitField.Flags).filter(
     (v) => typeof v === "number"
@@ -12,6 +15,20 @@ const client = new Client({
 
 client.config = require("./config");
 client.commands = new Collection();
+
+// register font for canvas use case
+const fontFiles = readdirSync(path.resolve(__dirname, "./assets/fonts/"));
+fontFiles.forEach((fontFile) => {
+    const split = fontFile.split(".")[0].split("-");
+    const family = split[0];
+    const weight = split[1]
+        ? Constants.FontWeight[split[1]]
+        : Constants.FontWeight.Regular;
+    registerFont(path.resolve(__dirname, `./assets/fonts/${fontFile}`), {
+        family,
+        weight,
+    });
+});
 
 const handlersPath = path.resolve(__dirname, "./handlers");
 readdirSync(handlersPath).forEach((fileName) =>
